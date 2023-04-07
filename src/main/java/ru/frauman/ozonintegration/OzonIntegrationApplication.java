aab4ab6fb0c7b2c820e826a71a183a991e7d73dc
HimmelSpark <adam404pet@gmail.com>
2023-04-07 23:25:46 +0300

    init

---
+++ src/main/java/ru/frauman/ozonintegration/OzonIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ru.frauman.ozonintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzonIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzonIntegrationApplication.class, args);
+	}
+
+}
